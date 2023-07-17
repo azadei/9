74b9659d893a4f0c933e01938422e5003d8f4d2a
azadei <azadei@code.edu.az>
2023-07-17 14:55:04 +0400

    1

---
+++ 9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        List<int> numbers = new List<int> { 55, 200, 740, 76, 230, 482, 95 };
+
+       
+        Console.WriteLine("The numbers greater than 80 are:");
+        foreach (int num in numbers)
+        {
+            if (num > 80)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+   
+    }
+}
+
+
+
